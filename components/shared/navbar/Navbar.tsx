a96f705f07aafeee2465171bf33c0fa39a61c5e7
Dikshit <dikshitwhite@gmail.com>
2024-05-05 21:31:36 +0530

    add mobilenav

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import Theme from './Theme';
+import MobileNav from './MobileNav';
 
 const Navbar = () => {
   return (
@@ -29,7 +30,7 @@ const Navbar = () => {
             }}
           />
         </SignedIn>
-        MobileNavbar
+        <MobileNav />
       </div>
     </nav>
   );
